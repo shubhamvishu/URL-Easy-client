4c622f58b7bea8714b26ab9097a44a7ba6c274f4
Shubham Chaudhary <shubhmasand@gmail.com>
2020-12-19 23:41:41 +0530

    Initialised repo

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get("*",(req,res)=>{
+    res.send("Server is working");
+});
+
+app.listen(process.env.PORT || 5000, ()=>{
+    console.log("Server listenong on port",process.env.PORT || 5000);
+});
