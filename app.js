1c459be8427a29cfdd707fb1fced9ed9258ba1ae
Shubham Chaudhary <shubhmasand@gmail.com>
2020-12-28 00:25:18 +0530

    start page

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.set("view engine","ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get("*",(req,res)=>{
-    res.send("Hello....Server is working");
+    res.render("home")
 });
 
 app.listen(process.env.PORT || 5000, ()=>{
